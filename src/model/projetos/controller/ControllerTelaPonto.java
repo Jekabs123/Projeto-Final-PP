7182907b3b88963d8cd91affd2ecbd00564ef469
paulo <paulo@10.0.0.117>
2020-11-10 16:30:37 -0300

    Tentei mudar a porta, mas não deu certo

--- src/model/projetos/controller/ControllerTelaPonto.java
+++ src/model/projetos/controller/ControllerTelaPonto.java
@@ -1,7 +1,10 @@
 package model.projetos.controller;
 
+import java.io.IOException;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.ServerSocket;
 import java.net.UnknownHostException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -17,7 +20,7 @@ public class ControllerTelaPonto {
 		//melhor deixar automático igual ao RegistradorPontoCentral/Server?
 		//Eu coloquei, mas não testei
 		try {
-			InterfaceAcessoRemotoPonto ponto = (InterfaceAcessoRemotoPonto) Naming.lookup("rmi://"+InetAddress.getLocalHost().getHostAddress()+":1099/PontoService");
+			InterfaceAcessoRemotoPonto ponto = (InterfaceAcessoRemotoPonto) Naming.lookup("rmi://"+InetAddress.getLocalHost().getHostAddress()+":1090/PontoService");
 			ponto.registrarPonto(projeto, login);
 		} catch (MalformedURLException | RemoteException | NotBoundException error) {
 			error.printStackTrace();
--- src/model/projetos/ponto/RegistradorPontoCentralServer.java
+++ src/model/projetos/ponto/RegistradorPontoCentralServer.java
@@ -1,5 +1,4 @@
 package model.projetos.ponto;
-
 import java.net.InetAddress;
 import java.rmi.Naming;
 import java.rmi.Remote;
@@ -7,11 +6,12 @@ import java.rmi.registry.LocateRegistry;
 
 public class RegistradorPontoCentralServer {
 	
+
 	public RegistradorPontoCentralServer() {
 		try {
-			
-			System.setProperty("java.rmi.server.hostname", InetAddress.getLocalHost().getHostAddress());
-			LocateRegistry.createRegistry(1099); //Colocar a porta
+
+			System.setProperty("java.rmi.server.hostname", InetAddress.getLocalHost().getHostName());
+			LocateRegistry.createRegistry(1090); //Colocar a porta
 			InterfaceAcessoRemotoPonto registrarPontoCentral = new RegistradorPontoCentral();
 			Naming.bind("PontoService", (Remote) registrarPontoCentral);
 		} catch (Exception e) {
