f9fadc931d861b61ee262c6df5ea3d13bfda2bf1
paulo <paulo@LAPTOP-AA5HVU43>
2020-12-05 23:13:07 -0300

    Fiz o ouvinte da tela configurarAdmin e fiz algumas coisas no controller membro

--- src/controller/ControllerMembro.java
+++ src/controller/ControllerMembro.java
@@ -7,10 +7,26 @@ import model.autenticacao.Membro;
 
 public class ControllerMembro {
 	
-	//private Fachada1Membro fachadaMembro = new Fachada1Membro();
+	private Fachada1Membro fachadaMembro = new Fachada1Membro();
 	
 	public ArrayList<Membro> getMembros() {
 		return Fachada1Membro.getMembros();
 	}
-
+	
+	public Membro pesquisar(long matricula){
+		return fachadaMembro.pesquisarMembro(matricula);
+	}
+	public void atualizar(){
+		fachadaMembro.atualizarMembro();
+	}
+	public void tornarMembroAdim(Membro membro){
+		if(getMembros().size()>0){
+			Membro admin = getMembros().get(0);
+			ControllerConfigurarAdim config = new ControllerConfigurarAdim(admin.getNome(), admin.getContaEmail(), admin.getEmail(), admin.getMatricula(), admin.getParticipacao());
+			config.tornarMembroAdim(membro);
+		}else{
+			ControllerConfigurarAdim config = new ControllerConfigurarAdim(membro.getNome(), membro.getContaEmail(), membro.getEmail(), membro.getMatricula(), membro.getParticipacao());
+			config.tornarMembroAdim(membro);
+		}
+	}
 }
--- src/view/autenticacao/swing/TelaConfiguracaoAdminSwing.java
+++ src/view/autenticacao/swing/TelaConfiguracaoAdminSwing.java
@@ -2,6 +2,8 @@ package view.autenticacao.swing;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -9,10 +11,13 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import controller.ControllerMembro;
+import model.autenticacao.Membro;
 import view.autenticacao.TelaConfiguracaoAdmin;
 
 public class TelaConfiguracaoAdminSwing extends JFrame implements TelaConfiguracaoAdmin {
 	
+	private JComboBox<String> listMembros;
 	public TelaConfiguracaoAdminSwing() {
 		setTitle("Configurações do Administrador");
 		setLayout(null);
@@ -44,7 +49,8 @@ public class TelaConfiguracaoAdminSwing extends JFrame implements TelaConfigurac
 	}
 	
 	public void addComboBox() {
-		JComboBox<String> listMembros = new JComboBox<String>();
+
+		listMembros = new JComboBox<String>();
 		listMembros.setBounds(150, 100, 200, 30);
 		add(listMembros);
 		
@@ -54,8 +60,24 @@ public class TelaConfiguracaoAdminSwing extends JFrame implements TelaConfigurac
 		JButton buttonTornarAdmin = new JButton("Tornar Administrador", new ImageIcon(getClass().getResource("/tornarAdmin.png")));
 		buttonTornarAdmin.setBackground(Color.gray);
 		buttonTornarAdmin.setBounds(160, 170, 180, 50);
+		OuvinteBotao ovuinte = new OuvinteBotao();
+		buttonTornarAdmin.addActionListener(ovuinte);
 		add(buttonTornarAdmin);
 		
 	}
-	
+	public class OuvinteBotao implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			long matricula = Long.parseLong((String)listMembros.getSelectedItem());
+			ControllerMembro controllerMembro = new ControllerMembro();
+			Membro membro = controllerMembro.pesquisar(matricula);
+			controllerMembro.tornarMembroAdim(membro);
+			controllerMembro.atualizar();
+		}
+		
+	}
+	public static void main(String[] args) {
+		new TelaConfiguracaoAdminSwing();
+	}
 }
